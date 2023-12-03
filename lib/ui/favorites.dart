109b04a15e35a45ead5d782657beab3c7248e3db
Kwame Agyapong <Kwameagyapong91@gmail.com>
2023-12-03 23:45:08 +0000

    ui and functionality for favorites 1

--- lib/ui/favorites.dart
+++ lib/ui/favorites.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class Favorites extends StatefulWidget {
   const Favorites({super.key});
@@ -8,12 +9,32 @@ class Favorites extends StatefulWidget {
 }
 
 class _FavoritesState extends State<Favorites> {
+  final _favBox = Hive.box("fav_box");
+  _deleteFav(int key) async {
+    await _favBox.delete(key);
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    List<dynamic> _fav = [];
+
     return Scaffold(
-        body: Text(
-      "Favorites",
-      style: appstyle,
+        body: SizedBox(
+      height: MediaQuery.of(context).size.height,
+      width: size.width,
+      child: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(16, 40, 0, 0),
+            height: size.height * 0.4,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/top_image.png'),
+                    fit: BoxFit.cover)),
+          ),
+        ],
+      ),
     ));
   }
 }
